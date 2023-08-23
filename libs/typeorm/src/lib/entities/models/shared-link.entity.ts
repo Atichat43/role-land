fdb139e814ebe15d7ae72ab90ecdc2021821a011
Atichat L <atichat.lap@gmail.com>
2023-08-23 22:39:35 +0700

    add idx sharedlink url

--- libs/typeorm/src/lib/entities/models/shared-link.entity.ts
+++ libs/typeorm/src/lib/entities/models/shared-link.entity.ts
@@ -3,6 +3,7 @@ import { SharedLink } from '../../core/models.types';
 import { BaseEntity } from '../_common.entity';
 
 @Entity('SharedLink')
+@Index('sharedLink_url_index', ['url']) // NOTE: query shared links by url
 @Index('sharedLink_isActive_index', ['isActive']) // NOTE: query shared links by isActive
 @Unique(['url'])
 export class SharedLinkEntity extends BaseEntity implements SharedLink {
