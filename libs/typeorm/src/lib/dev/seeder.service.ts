7c4ce4a8f68129b3ecb05686814be76f9f7ec29e
Atichat L <atichat.lap@gmail.com>
2023-08-26 23:20:54 +0700

    refactor seeder service

--- libs/typeorm/src/lib/dev/seeder.service.ts
+++ libs/typeorm/src/lib/dev/seeder.service.ts
@@ -1,102 +1,154 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { ObjectLiteral, Repository } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 
-import { AchievementEntity } from '../entities/achievement.entity';
-import { BadgeEntity } from '../entities/badge.entity';
-import { EffectEntity } from '../entities/effect.entity';
-import { PointEntity } from '../entities/point.entity';
-import { RoleEntity } from '../entities/role.entity';
-import { RolePreferenceEntity } from '../entities/role-preference.entity';
-import { SessionEntity } from '../entities/session.entity';
-import { SharedLinkEntity } from '../entities/shared-link.entity';
-import { ThemeEntity } from '../entities/theme.entity';
-import { UserEntity } from '../entities/user.entity';
+import * as Entities from '../entities';
 import * as SEED from './index.seed';
 import { OmitBaseEntityColumns } from './index.seed';
 
 @Injectable()
 export class SeederService {
+  repositories: {
+    PointEntity: Repository<Entities.PointEntity>;
+    SessionEntity: Repository<Entities.SessionEntity>;
+    UserEntity: Repository<Entities.UserEntity>;
+    AchievementEntity: Repository<Entities.AchievementEntity>;
+    BadgeEntity: Repository<Entities.BadgeEntity>;
+    EffectEntity: Repository<Entities.EffectEntity>;
+    RoleEntity: Repository<Entities.RoleEntity>;
+    SharedLinkEntity: Repository<Entities.SharedLinkEntity>;
+    ThemeEntity: Repository<Entities.ThemeEntity>;
+    RolePreferenceEntity: Repository<Entities.RolePreferenceEntity>;
+  };
+
   constructor(
-    @InjectRepository(PointEntity)
-    private readonly pointRepository: Repository<PointEntity>,
-    @InjectRepository(SessionEntity)
-    private readonly sessionRepository: Repository<SessionEntity>,
-    @InjectRepository(UserEntity)
-    private readonly userRepository: Repository<UserEntity>,
-    @InjectRepository(AchievementEntity)
-    private readonly achievementRepository: Repository<AchievementEntity>,
-    @InjectRepository(BadgeEntity)
-    private readonly badgeRepository: Repository<BadgeEntity>,
-    @InjectRepository(EffectEntity)
-    private readonly effectRepository: Repository<EffectEntity>,
-    @InjectRepository(RoleEntity)
-    private readonly roleRepository: Repository<RoleEntity>,
-    @InjectRepository(SharedLinkEntity)
-    private readonly sharedLinkRepository: Repository<SharedLinkEntity>,
-    @InjectRepository(ThemeEntity)
-    private readonly themeRepository: Repository<ThemeEntity>,
-    @InjectRepository(RolePreferenceEntity)
-    private readonly rolePreferenceRepository: Repository<RolePreferenceEntity>,
-  ) {}
-
-  async clearAll() {
-    Logger.log('▶ Clearing database...');
-
-    await this.clearPoints();
-    await this.clearSessions();
-    await this.clearBadges();
-    await this.clearAchievements();
-    await this.clearRolePreferences();
-    await this.clearUsers();
-    await this.clearRoles();
-    await this.clearSharedLinks();
-    await this.clearEffects();
-    await this.clearThemes();
-
-    Logger.log('✅ Clearing complete!');
+    @InjectRepository(Entities.PointEntity)
+    private readonly pointRepository: Repository<Entities.PointEntity>,
+    @InjectRepository(Entities.SessionEntity)
+    private readonly sessionRepository: Repository<Entities.SessionEntity>,
+    @InjectRepository(Entities.UserEntity)
+    private readonly userRepository: Repository<Entities.UserEntity>,
+    @InjectRepository(Entities.AchievementEntity)
+    private readonly achievementRepository: Repository<Entities.AchievementEntity>,
+    @InjectRepository(Entities.BadgeEntity)
+    private readonly badgeRepository: Repository<Entities.BadgeEntity>,
+    @InjectRepository(Entities.EffectEntity)
+    private readonly effectRepository: Repository<Entities.EffectEntity>,
+    @InjectRepository(Entities.RoleEntity)
+    private readonly roleRepository: Repository<Entities.RoleEntity>,
+    @InjectRepository(Entities.SharedLinkEntity)
+    private readonly sharedLinkRepository: Repository<Entities.SharedLinkEntity>,
+    @InjectRepository(Entities.ThemeEntity)
+    private readonly themeRepository: Repository<Entities.ThemeEntity>,
+    @InjectRepository(Entities.RolePreferenceEntity)
+    private readonly rolePreferenceRepository: Repository<Entities.RolePreferenceEntity>,
+  ) {
+    this.repositories = {
+      PointEntity: this.pointRepository,
+      SessionEntity: this.sessionRepository,
+      UserEntity: this.userRepository,
+      AchievementEntity: this.achievementRepository,
+      BadgeEntity: this.badgeRepository,
+      EffectEntity: this.effectRepository,
+      RoleEntity: this.roleRepository,
+      SharedLinkEntity: this.sharedLinkRepository,
+      ThemeEntity: this.themeRepository,
+      RolePreferenceEntity: this.rolePreferenceRepository,
+    };
   }
 
+  // Abstracted methods
+  private async seedEntity<T extends ObjectLiteral>(
+    entity: Repository<T>,
+    seedData: T[],
+  ) {
+    Logger.verbose(`Seeding ${entity.metadata.name}...`);
+    await entity.save(seedData);
+  }
+
+  private async clearEntity<T extends ObjectLiteral>(entity: Repository<T>) {
+    Logger.verbose(`Clearing ${entity.metadata.name}...`);
+    await entity.delete({});
+  }
+
+  // Seed All Data
   async seedAll() {
     Logger.log('▶ Seeding database...');
 
-    await this.seedThemes();
-    await this.seedEffects();
-    await this.seedSharedLinks();
-    await this.seedRoles();
-    await this.seedUsers();
-    await this.seedRolePreferences();
-    await this.seedAchievements();
-    await this.seedBadges();
-    await this.seedSessions();
-    await this.seedPoints();
+    const seedMap = {
+      ThemeEntity: SEED.themes,
+      EffectEntity: SEED.effects,
+      SharedLinkEntity: SEED.sharedLinks,
+      RoleEntity: SEED.roles,
+      UserEntity: SEED.users,
+      RolePreferenceEntity: SEED.rolePreferences,
+      AchievementEntity: SEED.achievements,
+      BadgeEntity: SEED.badges,
+      SessionEntity: SEED.sessions,
+      PointEntity: SEED.points,
+    };
+
+    for (const [entityName, seedData] of Object.entries(seedMap)) {
+      const repository = this.repositories[
+        entityName as keyof typeof this.repositories
+      ] as Repository<any>;
+      await this.seedEntity(repository, seedData as any);
+    }
 
     Logger.log('✅ Seeding complete!');
+  }
+
+  // Clear All Data
+  async clearAll() {
+    Logger.log('▶ Clearing database...');
+
+    const clearEntities = [
+      'PointEntity',
+      'SessionEntity',
+      'BadgeEntity',
+      'AchievementEntity',
+      'RolePreferenceEntity',
+      'UserEntity',
+      'RoleEntity',
+      'SharedLinkEntity',
+      'EffectEntity',
+      'ThemeEntity',
+    ];
+
+    for (const entityName of clearEntities) {
+      const repository = this.repositories[
+        entityName as keyof typeof this.repositories
+      ] as Repository<any>;
+      await this.clearEntity(repository);
+    }
+
+    Logger.log('✅ Clearing complete!');
   }
 
   async demoEventSubscribers() {
     Logger.log('▶ Demonstrating event subscribers...');
     // Earn Achievement (Triggers AchievementEarned Event)
-    const newAchievement: OmitBaseEntityColumns<AchievementEntity> = {
+    const newAchievement: OmitBaseEntityColumns<Entities.AchievementEntity> = {
       id: uuid(),
       achievementType: 'Master Player',
-      user: SEED.users[1] as UserEntity,
+      user: SEED.users[1] as Entities.UserEntity,
     };
     Logger.verbose('Creating achievement...');
     await this.achievementRepository.save(newAchievement);
 
     // Earn Badge (Triggers BadgeEarned Event)
-    const newBadge: OmitBaseEntityColumns<BadgeEntity> = {
+    const newBadge: OmitBaseEntityColumns<Entities.BadgeEntity> = {
       id: uuid(),
       badgeType: 'Platinum',
-      user: SEED.users[1] as UserEntity,
+      user: SEED.users[1] as Entities.UserEntity,
     };
     Logger.verbose('Creating badge...');
     await this.badgeRepository.save(newBadge);
 
     // Update Theme (Triggers ThemeUpdated Event)
-    const updatedTheme: Partial<ThemeEntity> = {
+    const updatedTheme: Partial<Entities.ThemeEntity> = {
       name: 'Epic Adventure',
       premium: true,
     };
@@ -104,7 +156,7 @@ export class SeederService {
     await this.themeRepository.update(SEED.themes[0].id, updatedTheme);
 
     // Earn Points (Triggers PointsEarned Event)
-    const pointsEarned: OmitBaseEntityColumns<PointEntity> = {
+    const pointsEarned: OmitBaseEntityColumns<Entities.PointEntity> = {
       ...SEED.points[0],
       pointsEarned: SEED.points[0].pointsEarned + 20,
       pointsBalance: SEED.points[0].pointsBalance + 20,
@@ -113,7 +165,7 @@ export class SeederService {
     await this.pointRepository.update(SEED.points[0].id, pointsEarned);
 
     // Spend Points (Triggers PointsSpent Event)
-    const pointsSpent: OmitBaseEntityColumns<PointEntity> = {
+    const pointsSpent: OmitBaseEntityColumns<Entities.PointEntity> = {
       ...SEED.points[0],
       pointsSpent: SEED.points[0].pointsSpent + 10,
       pointsBalance: SEED.points[0].pointsBalance - 10,
@@ -122,105 +174,5 @@ export class SeederService {
     await this.pointRepository.update(SEED.points[0].id, pointsSpent);
 
     Logger.log('✅ Demonstration complete!');
-  }
-
-  private async seedThemes() {
-    Logger.verbose('Seeding themes...');
-    await this.themeRepository.save(SEED.themes);
-  }
-
-  private async clearThemes() {
-    Logger.verbose('Clearing themes...');
-    await this.themeRepository.delete({});
-  }
-
-  private async seedEffects() {
-    Logger.verbose('Seeding effects...');
-    await this.effectRepository.save(SEED.effects);
-  }
-
-  private async clearEffects() {
-    Logger.verbose('Clearing effects...');
-    await this.effectRepository.delete({});
-  }
-
-  private async seedSharedLinks() {
-    Logger.verbose('Seeding shared links...');
-    await this.sharedLinkRepository.save(SEED.sharedLinks);
-  }
-
-  private async clearSharedLinks() {
-    Logger.verbose('Clearing shared links...');
-    await this.sharedLinkRepository.delete({});
-  }
-
-  private async seedRoles() {
-    Logger.verbose('Seeding roles...');
-    await this.roleRepository.save(SEED.roles);
-  }
-
-  private async clearRoles() {
-    Logger.verbose('Clearing roles...');
-    await this.roleRepository.delete({});
-  }
-
-  private async seedUsers() {
-    Logger.verbose('Seeding users...');
-    await this.userRepository.save(SEED.users);
-  }
-
-  private async clearUsers() {
-    Logger.verbose('Clearing users...');
-    await this.userRepository.delete({});
-  }
-
-  private async seedRolePreferences() {
-    Logger.verbose('Seeding role preferences...');
-    await this.rolePreferenceRepository.save(SEED.rolePreferences);
-  }
-
-  private async clearRolePreferences() {
-    Logger.verbose('Clearing role preferences...');
-    await this.rolePreferenceRepository.delete({});
-  }
-
-  private async seedAchievements() {
-    Logger.verbose('Seeding achievements...');
-    await this.achievementRepository.save(SEED.achievements);
-  }
-
-  private async clearAchievements() {
-    Logger.verbose('Clearing achievements...');
-    await this.achievementRepository.delete({});
-  }
-
-  private async seedBadges() {
-    Logger.verbose('Seeding badges...');
-    await this.badgeRepository.save(SEED.badges);
-  }
-
-  private async clearBadges() {
-    Logger.verbose('Clearing badges...');
-    await this.badgeRepository.delete({});
-  }
-
-  private async seedSessions() {
-    Logger.verbose('Seeding sessions...');
-    await this.sessionRepository.save(SEED.sessions);
-  }
-
-  private async clearSessions() {
-    Logger.verbose('Clearing sessions...');
-    await this.sessionRepository.delete({});
-  }
-
-  private async seedPoints() {
-    Logger.verbose('Seeding points...');
-    await this.pointRepository.save(SEED.points);
-  }
-
-  private async clearPoints() {
-    Logger.verbose('Clearing points...');
-    await this.pointRepository.delete({});
   }
 }
