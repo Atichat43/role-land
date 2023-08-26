083a57b4efa931f5487b7fec69e9510ed3c6602e
Atichat L <atichat.lap@gmail.com>
2023-08-26 14:42:21 +0700

    update doc

--- libs/domain/src/lib/_base.types.ts
+++ libs/domain/src/lib/_base.types.ts
@@ -1,8 +1,18 @@
+/**
+ * TimestampFields Domain Interface
+ * - createdDate: Date *(default: current date time)*
+ * - updatedDate: Date *(default: current date time)*
+ */
 export interface TimestampFields {
   createdDate: Date;
   updatedDate: Date;
 }
 
+/**
+ * SoftDeletionFields Domain Interface
+ * - isDeleted: boolean *(default: false)*
+ * - deletedDate: Date *(default: deleted date time)*
+ */
 export interface SoftDeletionFields {
   isDeleted: boolean;
   deletedDate: Date;
