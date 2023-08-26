3539affff2384e63640813b2e70940892dfd143e
Atichat L <atichat.lap@gmail.com>
2023-08-26 20:51:03 +0700

    cleanup

--- libs/typeorm/src/lib/entities/user.entity.ts
+++ libs/typeorm/src/lib/entities/user.entity.ts
@@ -1,6 +1,6 @@
 import { User } from '@role-land/domain';
 import { Type } from 'class-transformer';
-import { IsBoolean, IsInt, Length, ValidateNested } from 'class-validator';
+import { IsBoolean, Length, ValidateNested } from 'class-validator';
 import {
   Column,
   Entity,
@@ -14,7 +14,6 @@ import { AchievementEntity } from './achievement.entity';
 import { BadgeEntity } from './badge.entity';
 import { RolePreferenceEntity } from './role-preference.entity';
 import { ProfileEmbeded } from './user.profile.embed';
-import { RolePreferenceEmbeded } from './user.role-preference.embed';
 
 @Entity('User')
 // @Index('email_index', ['email'])
