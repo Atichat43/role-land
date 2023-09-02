43b49871ac983167a116bc31e960017ffa8adcb4
Atichat L <atichat.lap@gmail.com>
2023-09-02 21:28:37 +0700

    add doc on action

--- libs/domain/src/lib/core/action/action.enum.ts
+++ libs/domain/src/lib/core/action/action.enum.ts
@@ -1,3 +1,32 @@
+// --- Action Key Enum ---
+/**
+ * @desc Enum representing action keys.
+ * @values
+ * - Echo: Repeat the last action
+ * - ExtraTime: Add extra time to the timer
+ * - FreezeTime: Pause the timer
+ * - GiftTime: Give time to the current speaker
+ * - StealTime: Take time from the current speaker
+ * - TimeCut: Cut time from the current speaker
+ * - TimeBomb: Set a time bomb on the current speaker
+ * - HotPotato: Pass the current speaker role to another participant
+ * - RandomizeOrder: Randomize the speaking order
+ * - ReverseOrder: Reverse the speaking order
+ * - SkipTurn: Skip the current speaker
+ * - SwapRole: Swap the current speaker with another participant
+ * - FastForward: Fast forward the current speaker
+ * - Hide: Hide the current speaker
+ * - InstantReplay: Replay the last speaker
+ * - Joker: Joker
+ * - MirrorSkill: Mirror skill
+ * - PickPocket: Pick pocket
+ * - Silencer: Silencer
+ * - Veto: Veto
+ * - Wildcard: Wildcard
+ * - Blank: Blank
+ * @default Blank
+ * @note Blank is a special action that does nothing
+ */
 export enum EActionKey {
   Echo = 'echo',
   ExtraTime = 'extra_time',
@@ -23,6 +52,15 @@ export enum EActionKey {
   Blank = 'blank',
 }
 
+// --- Action Type Enum ---
+/**
+ * @desc Enum representing action types.
+ * @values
+ * - Timer: Timer actions
+ * - Role: Role actions
+ * - Ordering: Ordering actions
+ * - Strategic: Strategic actions
+ */
 export enum EActionType {
   Timer = 'timer',
   Role = 'role',
@@ -30,6 +68,17 @@ export enum EActionType {
   Strategic = 'strategic',
 }
 
+// --- Timer Action Operation Enum ---
+/**
+ * @desc Enum representing timer action operations.
+ * @values
+ * - Add: Add time to the timer
+ * - Subtract: Subtract time from the timer
+ * - Pause: Pause the timer
+ * - Reset: Reset the timer
+ * - Set: Set the timer
+ * - Halve: Halve the timer
+ */
 export enum ETimerActionOperation {
   Add = 'add',
   Subtract = 'subtract',
@@ -39,17 +88,46 @@ export enum ETimerActionOperation {
   Halve = 'halve',
 }
 
+// --- Role Action Operation Enum ---
+/**
+ * @desc Enum representing role action operations.
+ * @values
+ * - Swap: Swap the current speaker with another participant
+ * - Randomize: Randomize the speaking order
+ */
 export enum ERoleActionOperation {
   Swap = 'swap',
   Randomize = 'randomize',
 }
 
+// --- Ordering Action Operation Enum ---
+/**
+ * @desc Enum representing ordering action operations.
+ * @values
+ * - Randomize: Randomize the speaking order
+ * - Reverse: Reverse the speaking order
+ * - Skip: Skip the current speaker
+ */
 export enum EOrderingActionOperation {
   Randomize = 'randomize',
   Reverse = 'reverse',
   Skip = 'skip',
 }
 
+// --- Strategic Action Operation Enum ---
+/**
+ * @desc Enum representing strategic action operations.
+ * @values
+ * - FastForward: Fast forward the current speaker
+ * - Hide: Hide the current speaker
+ * - InstantReplay: Replay the last speaker
+ * - Joker: Joker
+ * - MirrorSkill: Mirror skill
+ * - PickPocket: Pick pocket
+ * - Silencer: Silencer
+ * - Veto: Veto
+ * - Wildcard: Wildcard
+ */
 export enum EStrategicActionOperation {
   FastForward = 'fast_forward',
   Hide = 'hide',
@@ -62,6 +140,23 @@ export enum EStrategicActionOperation {
   Wildcard = 'wildcard',
 }
 
+// --- Target Enum ---
+/**
+ * @desc Enum representing targets.
+ * @values
+ * - NextSpeaker: Targeting the next speaker
+ * - PreviousSpeaker: Targeting the previous speaker
+ * - CurrentSpeaker: Targeting the current speaker
+ * - AllParticipants: Targeting all participants
+ * - AllSpeakers: Targeting all speakers
+ * - AllListeners: Targeting all listeners
+ * - OtherParticipants: Targeting other participants
+ * - TargetParticipant: Targeting a specific participant
+ * - Self: Targeting self
+ * - BackgroundCountdown: Targeting the background countdown (timer)
+ * - Custom: Targeting a custom target
+ * @note Custom targets are set dynamically
+ */
 export enum ETargetEnum {
   // Targeting Speaker
   NextSpeaker = 'next_speaker',
